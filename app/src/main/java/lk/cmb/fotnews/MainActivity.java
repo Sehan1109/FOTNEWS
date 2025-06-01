a2572248e0f7442693d12376146c433b3c3c1c88
Sehan1109 <2022t01533@stu.cmb.ac.lk>
2025-06-01 09:07:25 +0530

    add news_screen layout

--- app/src/main/java/lk/cmb/fotnews/MainActivity.java
+++ app/src/main/java/lk/cmb/fotnews/MainActivity.java
@@ -1,6 +1,8 @@
 package lk.cmb.fotnews;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,11 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(MainActivity.this, Signin_Screen.class);
+            startActivity(intent);
+            finish(); // Finish loading screen so user can't go back to it
+        }, 2000);
     }
 }
