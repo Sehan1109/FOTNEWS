2c39ee8c5408c5ef81f0c8c1553d0f4d63eded19
Sehan1109 <2022t01533@stu.cmb.ac.lk>
2025-06-06 22:02:13 +0530

    add news in bottom navigation bar menu

--- app/src/main/java/lk/cmb/fotnews/News_Screen.java
+++ app/src/main/java/lk/cmb/fotnews/News_Screen.java
@@ -3,6 +3,7 @@ package lk.cmb.fotnews;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -14,6 +15,7 @@ import androidx.core.view.WindowInsetsCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
@@ -29,6 +31,7 @@ public class News_Screen extends AppCompatActivity {
     List<NewsItem> newsList = new ArrayList<>();
     TextView welcomemsg;
 
+    @SuppressLint("NonConstantResourceId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -59,6 +62,111 @@ public class News_Screen extends AppCompatActivity {
                 for (DataSnapshot child : snapshot.getChildren()) {
                     NewsItem item = child.getValue(NewsItem.class);
                     newsList.add(item);
+                }
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottomNav);
+        bottomNav.setOnItemSelectedListener(new BottomNavigationView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                if (id == R.id.nav_all) {
+                    loadNews();
+                    return true;
+                } else if (id == R.id.nav_academics) {
+                    loadAcademics();
+                    return true;
+                } else if (id == R.id.nav_sports) {
+                    loadSports();
+                    return true;
+                } else if (id == R.id.nav_events) {
+                    loadEvents();
+                    return true;
+                }
+                return false;
+            }
+        });
+    }
+
+    private void loadNews() {
+        FirebaseDatabase.getInstance().getReference("news").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                newsList.clear();
+                for (DataSnapshot child : snapshot.getChildren()) {
+                    NewsItem item = child.getValue(NewsItem.class);
+                    if (item != null) {
+                        newsList.add(item); // ✅ add ALL news items regardless of type
+                    }
+                }
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+            }
+        });
+    }
+
+    private void loadAcademics() {
+        FirebaseDatabase.getInstance().getReference("news").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                newsList.clear();
+                for (DataSnapshot child : snapshot.getChildren()) {
+                    NewsItem item = child.getValue(NewsItem.class);
+                    if("Academics".equals(child.child("type").getValue(String.class))) {
+                        newsList.add(item);
+                    }
+                }
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    private void loadSports() {
+        FirebaseDatabase.getInstance().getReference("news").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                newsList.clear();
+                for (DataSnapshot child : snapshot.getChildren()) {
+                    NewsItem item = child.getValue(NewsItem.class);
+                    if("Sports".equals(child.child("type").getValue(String.class))) {
+                        newsList.add(item);
+                    }
+                }
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    private void loadEvents() {
+        FirebaseDatabase.getInstance().getReference("news").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                newsList.clear();
+                for (DataSnapshot child : snapshot.getChildren()) {
+                    NewsItem item = child.getValue(NewsItem.class);
+                    if("Events".equals(child.child("type").getValue(String.class))) {
+                        newsList.add(item);
+                    }
                 }
                 adapter.notifyDataSetChanged();
             }
